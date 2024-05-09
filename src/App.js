78a59f113b1bd0adaca34ffd6044c1d3f93c4df1
Revanth709 <battularevanth29@gmail.com>
2024-05-09 20:10:28 +0530

    version 1

---
+++ src/App.js
@@ -0,0 +1,46 @@
+import './App.css';
+
+
+function App() {
+  return (
+ 
+    <>
+
+      <div className='container'>
+{/* .....................logo............................. */}
+        <div className='logo'>
+
+          <h1>LOGO NAME</h1>
+        
+        </div>
+ {/* .....................main-navigation............................. */}
+
+        <div className='main-nav'>
+
+          <ul className='navbar-items'>
+            <li>home</li>
+            <li>contact us</li>
+            <li>about us</li>
+            <li>cart</li>
+          </ul>
+
+        </div>
+   {/* .....................icons............................. */}
+
+        <div className='icons'>
+          <ul className='iconbar'>
+            <li>login</li>
+            <li>sign up</li>
+            <li>profile</li>
+
+          </ul>
+        </div>
+
+
+      </div>
+
+    </>
+  );
+}
+
+export default App;
---
+++ src/Navbar.jsx
@@ -0,0 +1,42 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <>
+     {/* .........................nabvar.......................... */}
+
+     <div className='container'>
+
+<div className='logo'><h2>LOGO</h2></div>
+
+<div className='main-nav'>
+  <ul className='nav-items'> 
+    <li>HOME</li>
+    <li>ABOUT US</li>
+    <li>CONTACT US</li>
+    <li> CART</li>
+  </ul>
+</div>
+
+<div className='icons'>
+  <ul className='icon-items'>
+    <li>profile</li>
+    <li>login in</li>
+    <li>sign up</li>
+  </ul>
+</div>
+
+</div>
+
+
+{/* .........................nabvar.......................... */}
+
+<h1>practice daily</h1>
+    
+    
+    
+    </>
+  )
+}
+
+export default Navbar
